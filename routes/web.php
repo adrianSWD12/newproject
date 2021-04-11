a7ab1ac3e43fc3dbd8e16e9cd3dbee12b9c6e34b
adrianSWD12 <78161230+adrianSWD12@users.noreply.github.com>
2021-04-11 15:17:40 -0500

    Set up build process

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/articles/create', 'App\http\Controllers\ArticlesController@create')
 Route::get('/articles/{article}', 'App\Http\Controllers\ArticlesController@show')->name('articles.show');
 Route::get('/articles/{article}/edit', 'App\Http\Controllers\ArticlesController@edit');
 Route::put('/articles/{article}', 'App\Http\Controllers\ArticlesController@update');
+Route::put('/articles/{article}', 'App\Http\Controllers\ArticlesController@update');
 
 
 
